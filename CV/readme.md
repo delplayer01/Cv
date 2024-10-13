1fdb434e9cfc4685de17db28f2c9bc4f20dcd25a
delplayer01 <142465465+delplayer01@users.noreply.github.com>
2024-10-13 16:34:12 -0500

    Add files via upload

---
+++ CV/readme.md
@@ -0,0 +1,3 @@
+Portafolio de delplayer01
+
+si en gitub y que? no me quiero gastar el dinero en un server y un dominio ja
---
+++ CV/script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+   
+    setTimeout(function () {
+        
+        document.getElementById("loader").style.display = "none";
+        
+        document.getElementById("content").style.display = "block";
+    }, 5000);
+};
